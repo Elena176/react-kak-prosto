28439e071584abf7bdd30d76ce8acd144da63c80
Elena176 <lena.tdn176@gmail.com>
2021-05-31 20:17:55 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import Rating from './components/Rating/Rating';
+import OnOff from './components/OnOff/OnOff';
+
+function App() {
+  console.log('App rendering');
+  //полезное что-то
+
+  //обязана вернуть JSX
+  return (
+    <div>
+      <PageTitle title={'Hello, Elena'}/>
+      <PageTitle title={'My friends'}/>
+      <Rating value={3}/>
+     <Accordion titleValue={'Menu'} collapsed={true}/>
+      <Accordion titleValue={'Users'} collapsed={false}/>
+     <Rating value={0}/>
+      <Rating value={1}/>
+      <Rating value={2}/>
+      <Rating value={3}/>
+      <Rating value={4}/>
+      <Rating value={5}/>
+        <OnOff />
+        <OnOff  />
+        <OnOff  />
+    </div>
+  );
+}
+
+type PageTitlePropsType = {
+  title: string
+}
+function PageTitle(props: PageTitlePropsType) {
+  console.log('AppTitle rendering');
+  return <h1>{ props.title }</h1>
+}
+
+
+
+
+export default App;
---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import './OnOff.css'
+
+type ButtonPropsType = {
+  // turn: boolean
+}
+
+
+function OnOff(props: ButtonPropsType) {
+
+    let [turn, setTurn] = useState(false);
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: turn ? 'aquamarine' : 'white'
+    };
+
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '2px',
+        padding: '2px',
+        backgroundColor: turn ? 'white' : 'red'
+    };
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '5px',
+        backgroundColor: turn ? 'aquamarine' : 'red'
+    };
+
+    return (
+        <div>
+            <div style={onStyle} onClick={ () => {setTurn(true)}}> On</div>
+            <div style={offStyle} onClick={ () => {setTurn(false)}}>Off</div>
+            <div style={indicatorStyle}>{}</div>
+            {/*<div className={'switchOn'}> On</div>
+            <div className={'switchOff'}>Off</div>
+            <div className={'indicator'}>{}</div>*/}
+        </div>
+    )
+}
+
+export default OnOff;
